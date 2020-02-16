3d7b443cd50131a014e21a5b3cfa572623a20f56
gautamrealzero <gautam.zero@gmail.com>
2020-02-16 20:33:00 +0600

    allow headers

--- app/routes/project.routes.js
+++ app/routes/project.routes.js
@@ -11,14 +11,6 @@ module.exports = (app, multer) => {
   })
   const upload = multer({ storage: storage })
 
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-    next();
-  });
-
   app.post('/projects', auth.checkToken, upload.single('projectImage'), projects.create);
 
   app.get('/projects', projects.findAll);
--- server.js
+++ server.js
@@ -30,6 +30,14 @@ app.get('/', (req, res) => {
     });
 });
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    next();
+  });
+
 // Require Notes routes
 require('./app/routes/auth.routes.js')(app);
 require('./app/routes/note.routes.js')(app, multer);
